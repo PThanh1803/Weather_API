40196b47bc4f1f7af515fd1e13e57519ac8cc3f8
Pham Ba Thanh <93045641+PThanh1803@users.noreply.github.com>
2024-07-18 17:07:10 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/weather', weatherRouter);
 app.use('/api/email', emailRoute);
 
 // send daily emails
-cron.schedule('59 11 * * *', async () => {
+cron.schedule('10 7 * * *', async () => {
     console.log('running a task every minute');
     await sendDailyEmails();
   });
